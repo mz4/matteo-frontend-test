22206c0bed0673f1f40111d264b9402a45f7dc73
Matteo <matteozaratti@gmail.com>
2021-05-15 09:17:45 +0100

    T1 Add table from local JSON data

---
+++ src/components/Metrics.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import metricsData from '../utils/Metrics';
+
+const Metrics = () => {
+  const [data, setData] = useState(metricsData);
+  return (
+    <>
+      <table>
+        <tr>
+          <th>
+            Metrics
+          </th>
+          <th>
+            Value
+          </th>
+        </tr>
+        <tbody>
+          {data.map((m) => 
+            (
+              <tr>
+                <td>{m.Metric}</td>
+                <td>{m.Values.slice(-1)}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Metrics;
---
+++ src/utils/Metrics.js
@@ -0,0 +1,20 @@
+const metricsData = [
+  {
+    "Metric": "MetricA",
+    "Values": [1,2,3,4,5]
+  },
+  {
+    "Metric": "MetricB",
+    "Values": [1,2,3,4,5,7]
+  },
+  {
+    "Metric": "MetricC",
+    "Values": [1,2,3,4,5,8,9]
+  },
+  {
+    "Metric": "MetricD",
+    "Values": []
+  }
+]
+
+export default metricsData;
